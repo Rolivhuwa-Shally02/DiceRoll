eace806cf50b0c65986608a6f0fffe4abac6dadd
Rolivhuwa-Shally02 <80606439+Rolivhuwa-Shally02@users.noreply.github.com>
2023-05-22 16:24:25 +0200

    Rolling dices rasndomly

--- NewDiceApp/ViewController.swift
+++ NewDiceApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        TableView.dataSource = self
     }
 
 
 }
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection Section:
+                   Int) -> Int{
+        return 10
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "I am a cell"
+        return cell
+    }
+    
+  
+    }
+
+
+    
+
 
